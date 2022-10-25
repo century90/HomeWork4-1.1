01804bb652a0f340ec69698324f8bb870b085836
century90 <century90@mail.ru>
2022-10-25 12:47:01 +0300

    first commit

---
+++ src/main/java/ru/netology/BonusService.java
@@ -0,0 +1,12 @@
+package ru.netology;
+
+
+public class BonusService {
+
+    private final int boundary = 1000;
+
+    public int remain(int amount) {
+        return boundary - amount % boundary;
+    }
+
+}
